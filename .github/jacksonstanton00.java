ec1e58813563af574e9b9aa856052b0ec2e284d2
s11028834 <s11028834@students.duvalschools.org>
2021-12-08 09:33:19 -0500

    Create jacksonstanton00.java

---
+++ .github/jacksonstanton00.java
@@ -0,0 +1,25 @@
+pubic
+class JackStanton00 {
+
+    public static void main(String[] args){
+        // creating an array by immediately assigning the values of the indexes
+        string[] strArr = {"imperious", "deride","hackneyed", "immolate", "grotesque", "censure", "gambol",
+    "quagmire", "gibe", "martinet"};
+
+    int[] intArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    //creating an array by assigning with a for-loop
+    //the elements of this double array should be values 0-9 and not 1-10 because the variable i starts at 0 and ends at 9
+    double[] doubArr = new double[10];
+    for(int i = 0; i < doubArr.length; i++){
+        doubArr[i] = i;
+    }
+
+    public static void stringArray(String[] strArr){
+        for(int i = 0; i < strArr.length; i++){
+            System.out.pprintln(strArr[i]); //prints the String
+
+            int remainder = strArr[i].length() % 2
+        }
+      }
+}}
